3bd51dfb5a50abc53890ea8edaab71bd090fb937
Peter Osoo <posoo617@gmail.com>
2023-07-15 12:06:14 +0300

    improve UI for single Todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,12 @@
+import { LiaCheckSolid, LiaTrashSolid, LiaPenSolid } from "react-icons/lia"
+
 const Todo = () => {
 	return (
 		<div class="card">
-			<div class="card-body">Single Todo</div>
+			<div class="card-body">
+				<span>Todo</span> <LiaPenSolid /> <LiaCheckSolid /> <LiaTrashSolid />
+			</div>
+			{/* <span class="bi bi-trash"></span> */}
 		</div>
 	)
 }
