7b550d3cd4de596c8835d2685ccc79c653587d96
Peter Osoo <posoo617@gmail.com>
2023-08-08 08:57:35 +0300

    FIlter Button

---
+++ src/components/FilterButton.jsx
@@ -0,0 +1,17 @@
+const FilterButton = () => {
+	return (
+		<div className="category-buttons my-3">
+			<a href="#" className="btn btn-outline-dark">
+				All
+			</a>
+			<a href="#" className="btn btn-outline-info mx-3">
+				Active
+			</a>
+			<a href="#" className="btn btn-outline-secondary">
+				Completed
+			</a>
+		</div>
+	)
+}
+
+export default FilterButton
--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,24 +1,16 @@
 import Todo from "./Todo"
+import FIlterButton from "./FIlterButton"
 
 const TodoContainer = () => {
 	return (
 		<div>
 			<h3 className="my-3">Todo List</h3>
-			<div className="category-buttons my-3">
-				<a href="#" className="btn btn-outline-dark">
-					All
-				</a>
-				<a href="#" className="btn btn-outline-info mx-3">
-					Active
-				</a>
-				<a href="#" className="btn btn-outline-secondary">
-					Completed
-				</a>
-			</div>
+			<FIlterButton />
 			<h5>3 tasks remaining</h5>
 			<Todo name="Eat" completed={true} id="todo-0" />
 			<Todo name="Sleep" completed={false} id="todo-1" />
 			<Todo name="Watch" completed={false} id="todo-2" />
+
 			<button type="button" className="btn btn-danger my-3">
 				Clear Todo{" "}
 			</button>
